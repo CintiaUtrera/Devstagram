a79c009ffcaadfce85ad9bca685a872170808f93
Cintia <cintiaa_utrera@outlook.es>
2024-05-20 21:04:04 -0300

    otra vista

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,7 @@
         </style>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <nav><a href="/nosotros">nosotros</a></nav>
         <h1>Hola laraveeeeeeel</h1>
     </body>
 </html>
