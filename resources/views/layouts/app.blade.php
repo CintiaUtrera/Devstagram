0fcdf69b7c4e26746df3258ada0abf94bcbbd6ca
Cintia <cintiaa_utrera@outlook.es>
2024-05-24 20:55:32 -0300

    creando footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,17 @@
                 </nav>
             </div>
         </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo') <!-- yield = directiva -->
+            </h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram - Todos los derechos reservados {{ now()->year }}
+        </footer>
     </body>
 
 </html>
