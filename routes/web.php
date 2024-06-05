d099c7e2fc2ade9e132a6ae910b6d00022b2a5f9
Cintia <cintiaa_utrera@outlook.es>
2024-06-05 15:00:00 -0300

    validacion al formulario login

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
-Route::get('/login', [LoginController::class, 'index'])->name('login');
 
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/muro', [PostController::class, 'index'])->name('posts.index')->middleware('auth');
