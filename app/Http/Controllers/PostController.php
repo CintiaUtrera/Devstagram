b52106eb02756c92dcf5cbe45be227d78857297e
Cintia <cintiaa_utrera@outlook.es>
2024-06-12 12:06:43 -0300

    url unica para los muros

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     
 
-    public function index()
+    public function index(User $user)
     {
+        
         return view('dashboard');
         
     }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
-
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
-Route::get('/muro', [PostController::class, 'index'])->name('posts.index')->middleware('auth');
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index')->middleware('auth');
