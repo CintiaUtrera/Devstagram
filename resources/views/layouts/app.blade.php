7e3578adb4f7a1b3959b9c48ad17113dd0a673b7
Cintia <cintiaa_utrera@outlook.es>
2024-06-13 20:36:52 -0300

    creando routing para cargar imagenes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
-        @vite('resources/js/app.js')
+        @stack('styles')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <title>DevStagram - @yield('titulo')</title>
     </head>
 
