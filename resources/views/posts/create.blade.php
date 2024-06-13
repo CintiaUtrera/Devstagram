42974cf777f69bffefb0df6a9c83094bec986155
Cintia <cintiaa_utrera@outlook.es>
2024-06-13 19:53:28 -0300

    finalizando formulario de publicacion

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,19 +9,34 @@
         <div class="md:w-1/2 px-10">
             Imagen aquí
         </div>
-        <div class="md:w-1/2 p-10  bg-white rounded-lg shadow-xl mt-10 md:mt-0">
+        <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
             <form action="{{ route('register') }}" method="POST">
                 @csrf <!-- CSRF es una directiva que registra un campo oculto, un input -->
                 <div class="mb-5">
-                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
-                        Nombre
+                    <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Titulo
                     </label>
-                    <input type="text" id="name" name="name" placeholder="Nombre" value="{{old ('name')}}"
-                            class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror" />
-                    @error('name')
+                    <input type="text" id="titulo" name="titulo" value="{{old ('titulo')}}"
+                            class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror" />
+                    @error('titulo')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
                     @enderror
                 </div>
+
+                <div class="mb-5">
+                    <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea id="descripcion" name="descripcion" class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror">
+                            {{old ('titulo')}}
+                    </textarea>
+                    @error('titulo')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <input type="submit" value="Publicar" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"  />
+
             </form>
         </div>
     </div>
