5799dd2d95b845e6590abf0a14431aa5d1d25fba
Cintia <cintiaa_utrera@outlook.es>
2024-07-01 19:41:44 -0300

    ejecutar y comprobar el factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,8 +19,8 @@ class PostFactory extends Factory
         return [
             'titulo' => $this->faker->sentence(5),
             'descripcion' => $this->faker->sentence(20),
-            'imagen' => $this->faker->uuid() . 'jpg',
-            'user_id' => $this->faker->randomElement([1, 2,])
+            'imagen' => $this->faker->uuid() . '.jpg',
+            'user_id' => $this->faker->randomElement([1])
         ];
     }
 }
