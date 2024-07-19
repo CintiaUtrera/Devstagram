5b7c2595b5fce2261e738b6c76c4ace7cacd594b
Cintia <cintiaa_utrera@outlook.es>
2024-07-19 12:30:52 -0300

    mostrar mensaje de forma condicional

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,6 +31,9 @@
 
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+
+        @if ($posts->count())
+
         <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             @foreach ($posts as $post)
                 <div>
@@ -40,6 +43,10 @@
                 </div>
             @endforeach
         </div>
+
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay publicaciones</p>
+        @endif
     
     </section>
 @endsection
