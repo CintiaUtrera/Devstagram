ea033076518766f85785597ba772212d11cc9fc2
Cintia <cintiaa_utrera@outlook.es>
2024-07-25 16:26:13 -0300

    mejorando las url

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -63,10 +63,13 @@ class PostController extends Controller
         return redirect()->route('posts.index', auth()->user()->username);
     }
 
-    public function show(Post $post)
+    public function show(User $user, Post $post)
     {
         return view('posts.show', [
-            'post' => $post
+            'post' => $post,
+            'user' => $user
+            
+
         ]);
     }
 }
